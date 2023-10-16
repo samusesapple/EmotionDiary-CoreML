f9d756d7ee34f6af68d8b1f0097482b06f376d48
Sam Sung <126672733+samusesapple@users.noreply.github.com>
2023-10-16 15:49:41 +0900

    [Feat] 새로운 DIary 추가하는 로직 구현

--- EmotionDiary/EmotionDiary/Views/AddDiaryAlert.swift
+++ EmotionDiary/EmotionDiary/Views/AddDiaryAlert.swift
@@ -9,61 +9,80 @@ import SwiftUI
 
 struct AddDiaryAlert: View {
     
-    @Binding var shouldPresentAlert: Bool
-    @Binding var title: String
-    @Binding var detail: String
+    @Binding var shouldAddNewDiary: Bool
     
-    init(shouldPresentAlert: Binding<Bool> = .constant(false), title: Binding<String> = .constant(""), detail: Binding<String> = .constant("")) {
-        self._shouldPresentAlert = shouldPresentAlert
-        self._title = title
-        self._detail = detail
-    }
+    @State var title: String = ""
+    @State var detail: String = ""
+    
+    var addDiaryButtonAction: ((Diary) -> Void)?
+    
     // MARK: - View
     var body: some View {
-        VStack(alignment: .center) {
-            HStack {
-                Text("새로운 일기 쓰기")
-                    .font(.system(size: 20, weight: .bold))
+        ZStack {
+            // 배경 흐리게 하기 위해
+            Color.black.opacity(0.75)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center) {
+                // 얼럿 상단
+                HStack {
+                    Text("새로운 일기 쓰기")
+                        .font(.system(size: 20, weight: .bold))
+                    
+                    Spacer()
+                    
+                    Button {
+                        withAnimation {
+                            shouldAddNewDiary = false
+                        }
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                }
+                .frame(width: 300)
                 
-                Spacer()
+                TextField("일기 제목", text: $title)
+                    .frame(width: 300)
+                    .padding(.all, 8)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray.opacity(0.5))
+                    }
                 
+                TextField("일기 내용", text: $detail, axis: .vertical)
+                    .lineLimit(8...12)
+                    .frame(width: 300)
+                    .padding(.all, 8)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray.opacity(0.5))
+                    }
+                
+                // 일기 추가 버튼
                 Button {
-                    print("""
-제목: \(title)
-내용: \(detail)
-""")
+                    let newDiary = Diary(title: title,
+                                         detail: detail,
+                                         emotion: .soso)
+                    addDiaryButtonAction!(newDiary)
                 } label: {
-                    Image(systemName: "xmark")
+                    Text("일기 추가")
+                        .frame(width: 90, height: 38)
+                        .foregroundColor(.white)
+                        .background(!title.isEmpty ? Color.blue : Color.gray.opacity(0.2)) // 제목 비어있는 여부에 따라 일기 추가 버튼 상태 색 설정
+                        .cornerRadius(10)
                 }
+                .disabled(title.isEmpty) // 제목이 비어있으면 일기 추가 버튼 disabled
+                .padding(.top)
             }
-            .frame(width: 300)
-            
-            TextField("일기 제목", text: $title)
-                .frame(width: 300)
-                .padding(.all, 8)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.gray.opacity(0.5))
-                }
-            
-            TextField("일기 내용", text: $detail, axis: .vertical)
-                .lineLimit(8...12)
-                .frame(width: 300)
-                .padding(.all, 8)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.gray.opacity(0.5))
-                }
-            
-        }
-        .padding()
-        .background(.white)
+            .padding()
+            .background()
         .cornerRadius(10)
+        }
     }
 }
 
 struct AddDiaryAlert_Previews: PreviewProvider {
     static var previews: some View {
-        AddDiaryAlert()
+        AddDiaryAlert(shouldAddNewDiary: Binding<Bool>.constant(false))
     }
 }
--- EmotionDiary/EmotionDiary/Views/MainView.swift
+++ EmotionDiary/EmotionDiary/Views/MainView.swift
@@ -9,36 +9,51 @@ import SwiftUI
 
 struct MainView: View {
     
-    @State var diaryList: [Diary] = Diary.mockDiaries
+    @State var diaryList: [Diary] = []
     @State var shouldAddNewDiary: Bool = false // 새로운 일기 추가 얼럿 띄우기 여부
     
     // MARK: - View
     var body: some View {
         NavigationStack {
-            VStack {
-                DiaryListView(diaryList: diaryList)
-                    .padding(.top)
-                Text("일기 \(diaryList.count)개")
-                    .font(.system(size: 14))
-                    .foregroundColor(Color.gray.opacity(0.9))
-            }
-            .navigationTitle("감정 일기")
-            // 탭바
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink {
-                        Text("새로운 일기 추가하는 view")
-                    } label: {
-                        Image(systemName: "plus")
-                            .bold()
+            ZStack {
+                VStack {
+                    DiaryListView(diaryList: diaryList)
+                        .padding(.top)
+                    Text("일기 \(diaryList.count)개")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color.gray.opacity(0.9))
+                }
+                .navigationTitle("감정 일기")
+                // 탭바
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            withAnimation {
+                                shouldAddNewDiary.toggle()
+                            }
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .disabled(shouldAddNewDiary) // 얼럿 뜬 상태에서 버튼 비활성화 (사용자 액션 제어)
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        NavigationLink {
+                            Text("여태 쓴 일기의 감정 통계 보여주는 차트")
+                        } label: {
+                            Image(systemName: "chart.pie.fill")
+                        }
+                        .disabled(shouldAddNewDiary) // 얼럿 뜬 상태에서 버튼 비활성화 (사용자 액션 제어)
                     }
                 }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    NavigationLink {
-                        Text("여태 쓴 일기의 감정 통계 보여주는 차트")
-                    } label: {
-                        Image(systemName: "chart.pie.fill")
-                            .foregroundColor(.green)
+                
+                // 일기 추가하기 얼럿
+                if shouldAddNewDiary {
+                    AddDiaryAlert(shouldAddNewDiary: $shouldAddNewDiary) { newDiary in
+                        self.diaryList.append(newDiary)
+                        withAnimation {
+                            shouldAddNewDiary.toggle()
+                        }
                     }
                 }
             }
